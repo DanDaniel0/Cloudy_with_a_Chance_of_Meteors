7c5f43ef175beebe3b0ab930f5509770dd7992a1
Paul <superduperpacman42@gmail.com>
2019-04-10 22:18:56 -0400

    Changed data format to points array

--- data_visualizer.py
+++ data_visualizer.py
@@ -2,19 +2,29 @@ import pandas
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+import math
+
+class Point:
+	def __init__(self, x, y, refl, height):
+		self.x = x
+		self.y = y
+		self.refl = refl
+		self.height = height
+
+def get(points, key):
+	return [getattr(p,key) for p in points]
 
 dat=pandas.read_csv('KLOT_ParkForest_Refl.csv')
 # data=pandas.read_csv('KLOT_ParkForest_Rvel.csv')
 N = len(dat)
-dat = dat[dat['elevAngle'] > 9]
-dat['x'] = pandas.Series(range(N))
-dat['y'] = pandas.Series(range(N))
-
-for i in range(2633, N-2632):
-	print(dat['longitude'][:])
-	dat['x'][i] = (dat['longitude'][i]-['longitude'][2633])*111*np.cosd(dat['latitude'][i]) # km
-	dat['y'][i] = (dat['latitude'][i]-['latitude'][2633])*111 # km
-
+points = []
+for i in range(N):
+	if dat['elevAngle'][i] < 9:
+		continue
+	x = (dat['longitude'][i]-dat['longitude'][0])*111*math.cos(math.radians(dat['latitude'][i])) # km
+	y = (dat['latitude'][i]-dat['latitude'][0])*111 # km
+	p = Point(x, y, dat['value'][i], dat['heightRel'][i])
+	points.append(p)
 
 # data = {'refl':dat['value'], 'lat':np.zeros(N), 'lon':np.zeros(N), 'height':dat['heightRel']}
 # Compute mean lat/lon of each rectangle
@@ -28,6 +38,6 @@ for i in range(2633, N-2632):
 
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', projection='3d')
-ax.scatter(dat['x'], dat['y'], dat['heightRel'], c=dat['value'])
+ax.scatter(get(points, 'x'), get(points, 'y'), get(points, 'height'), c=get(points, 'refl'))
 
 plt.show()
